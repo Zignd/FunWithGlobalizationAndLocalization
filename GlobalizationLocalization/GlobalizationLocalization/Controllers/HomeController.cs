826dc15977a7e1180442f69eb4d2636e197b83f2
Igor Hipólito Vieira <zignd.igor@gmail.com>
2015-10-22 11:24:46 -0200

    The entire project

---
+++ GlobalizationLocalization/GlobalizationLocalization/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using GlobalizationLocalization.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GlobalizationLocalization.Controllers
+{
+    public class HomeController : Controller
+    {
+        private static List<Demo> _list = new List<Demo>();
+
+        public ActionResult Index()
+        {
+            ViewBag.List = _list;
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Demo model)
+        {
+            if (ModelState.IsValid)
+            {
+                _list.Add(model);
+                return RedirectToAction("Index");
+            }
+
+            return View();
+        }
+    }
+}
---
+++ GlobalizationLocalization/GlobalizationLocalization/Models/Demo.cs
@@ -0,0 +1,21 @@
+﻿using MyResources;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace GlobalizationLocalization.Models
+{
+    public class Demo
+    {
+        [Display(Name = "Name", ResourceType = typeof(Resources))]
+        [Required(ErrorMessageResourceName = "NameRequiredError", ErrorMessageResourceType = typeof(Resources))]
+        public string Name { get; set; }
+
+        [Display(Name = "Age", ResourceType = typeof(Resources))]
+        [Required(ErrorMessageResourceName = "AgeRequiredError", ErrorMessageResourceType = typeof(Resources))]
+        public int Age { get; set; }
+    }
+}
